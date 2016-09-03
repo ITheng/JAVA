4b6c04ed318cd8c08b877054191efe54cdb4b63a
ITheng <15514920226@sina.cn>
2016-09-03 21:01:33 +0800

    c=3

---
+++ src/org/line/java/C4.java
@@ -0,0 +1,12 @@
+package org.line.java;
+
+public class C4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=1;
+int b=2;
+int c=a+b;
+System.out.print(c);
+
+	}}
